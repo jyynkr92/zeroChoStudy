bb6d377a4b357c7c13f861fc63824820b0aab6a7
aliceyoo <jyynkr92@gmail.com>
2019-08-07 08:56:52 +0900

    shouldComponentUpdate!

--- baseballgame/Try.jsx
+++ baseballgame/Try.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+//shouldComponentUpdate -- 바뀌는게 없으면 렌뎌링 안되도록 해주는게 필요함!
 
-const Try = ({tryInfo}) => {
+const Try = ({tryInfo}) => { //props는 많이 rendering이 되기 때문에 에러가 날 가능성이 많은 곳임!
     return (
         <li> {/** 한 component에서 다른 component로 옮길때, props를 사용 */}
             <div>{tryInfo.try}</div>
