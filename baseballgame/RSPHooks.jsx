27eb44810e70cd65e0d64ed65868dad34b5ffca0
aliceyoo <jyynkr92@gmail.com>
2019-08-20 08:59:53 +0900

    differences between class and hooks in life cycle

--- baseballgame/RSPHooks.jsx
+++ baseballgame/RSPHooks.jsx
@@ -26,14 +26,44 @@ const RSP = () => {
     const [score, setScore] = useState(0);
     const interval = useRef();
 
+    //useLayoutEffect() -> 화면의 resizing 되기 전에 ! (화면 바뀌는걸 감지하는 역할)
+    //useEffect() -> 화면이 resizing된 후에 실행됨
+
+    /** useEffect는 세로 기준, class에서는 가로 기준
+     *                          result,     imgCord,    score
+     * componentDidMount
+     * componentDidUpdate
+     * componentWillUnmount
+     * 
+     * 
+     * class일때
+     * componentDidMount() {
+     *   this.setState({
+     *      imgCord : 3,
+     *      score:1,
+     *      result :2,
+     *    })
+     * }
+     * 
+     * hooks일 때
+     * useEffect(() => {
+     *      setImgCord(3);
+     *      setScore(1);
+     * }, [imgCord, score]);
+     * 
+     * useEffect(() => {
+     *      setResult();
+     * }, [result]);
+     */
     //componentDidMount, componentDidUpdate, componentWillUnmount 역할을 모두 함
+    //useEffect는 여러번 사용 가능! (다른 효과를 내고싶을 때 각각 쓸 수 있음)
     useEffect(() => { //componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
         //매번 clearInterval하기 때문에 setInterval과 같은효과를 나타냄
         interval.current = setInterval(changeHand, 100);
         return () => { //componentWillUnmount역할
             clearInterval(interval.current);
         }
-    }, [imgCord]); //2번째 인자 배열에 넣은 값(imgCord)들이 바뀔 때, useEffect가 실행됨, 빈 배열인 경우 한번만 실행되고 안됨! (뭐가 바뀌어도 상관없다는 느낌임!)
+    }, [imgCord]); //2번째 인자 배열에 넣은 값(imgCord)들이 바뀔 때, useEffect가 실행됨, 빈 배열인 경우 한번만 실행되고 안됨! (뭐가 바뀌어도 상관없다는 느낌임! -- componentDidMount느낌임)
     
     const changeHand = () => {
         if (imgCord === rspCords.바위) {
